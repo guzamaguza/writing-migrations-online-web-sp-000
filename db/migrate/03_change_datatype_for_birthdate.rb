afe59ab5b730141d365edca9fdbf8a7ecce6cd4b
Mark A Guza <guzamaguza@gmail.com>
2020-04-05 04:51:25 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,8 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
     #add_column :students, :grade, :integer
-    change_column :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime do |t|
+    end
+
   end
 end
