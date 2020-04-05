fb0a827e505e1d4299688473d5220f3da3f1326f
Mark A Guza <guzamaguza@gmail.com>
2020-04-05 04:55:21 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,9 +1,6 @@
-class CreateStudents < ActiveRecord::Migration[5.2]
+class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-  create_table :artists do |t|
+  create_table :students do |t|
     t.string :name
-    t.string :genre
-    t.integer :age
-    t.string :hometown
   end
 end
